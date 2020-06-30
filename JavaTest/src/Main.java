51e635e209c974169b182659a6197c02fc4021a8
Broo2 <github@broo2.com>
2020-06-30 08:16:28 -0400

    Git test #1...

--- JavaTest/src/Main.java
+++ JavaTest/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the IntelliJ IDE");
+        System.out.println("update 1 for Git test");
     }
 }
