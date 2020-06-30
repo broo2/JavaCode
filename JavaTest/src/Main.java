a7256e9d3df03f9ecd45e23471ba049d43eeda98
Broo2 <github@broo2.com>
2020-06-30 12:24:28 -0400

    updated with control flow

--- JavaTest/src/Main.java
+++ JavaTest/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the IntelliJ IDE");
         System.out.println("update 1 for Git test");
+
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+
+        while(isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song of repeat?  If so, answer yes");
+
+            String userInput = input.next();
+            if(userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+            System.out.println("Playing next song...");
+        }
     }
 }
